4663d722357bcf5de0c7b0c8404730c61fc0876a
ChuprovKirill <k.chuproff@yandex.ru>
2016-12-07 15:25:13 +0300

    Initial commit

---
+++ src/main/java/ru/coursemodel/course/PassingCourse.java
@@ -0,0 +1,42 @@
+package main.java.ru.coursemodel.course;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.OptionalDouble;
+
+public class PassingCourse implements Serializable {
+    private List<Integer> marks = new ArrayList<>();
+
+    private Course course;
+
+    public PassingCourse(Course course) throws RuntimeException {
+        if (course == null) throw new RuntimeException("Укажите объект 'Курс'! ");
+        this.course = course;
+    }
+
+    public void addMark(int mark) {
+        if (TaskUtils.isValidMark(mark)) {marks.add(mark);}
+    }
+
+    public float getAverageMark() {
+        OptionalDouble average = marks
+                .stream()
+                .mapToDouble(a -> a)
+                .average();
+        return (average.isPresent() ? (float) average.getAsDouble() : 0);
+    }
+    public Integer getFinalMark() {
+
+        return Math.round(getAverageMark());
+    }
+
+    public Course getCourse() {
+        return this.course;
+    }
+
+    @Override
+    public String toString() {
+        return  "Оценки по текущему курсу" +"\n"+
+                marks;
+    }
+}
